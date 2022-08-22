c4b02bd7faceec5af876709f4990200b09d11b18
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2022-08-22 12:30:05 -0500

    validations

--- frontend/src/components/AI/Chatbot.jsx
+++ frontend/src/components/AI/Chatbot.jsx
@@ -7,9 +7,17 @@ const numberOfQuestions = 15;
 
 const SearchContainer = ({ question, answersButtons, onButtonClick }) => (
     <div className='justify-center'>
-        <h2 className='dark:text-white text-xl w-full flex justify-center'>
-            {question}
-        </h2>
+        {question ? (
+            <h2 className='dark:text-white text-xl w-full flex justify-center'>
+                {question}
+            </h2>
+        ) : (
+            <div>
+                <h2 className='dark:text-slate-300 text-2xl w-full flex justify-center py-5'>Cannot connect to Server</h2>
+                <div className="w-full flex justify-center"><img src={Error} alt="" /></div>
+            </div>
+        )}
+
         <div className='w-full flex justify-center p-5'>
             {answersButtons.map(answer => <button className='rounded-3xl bg-teal-600 text-white font-semibold py-2 px-16 hover:bg-slate-700 dark:hover:bg-slate-800 hover:text-white duration-300 inline-flex items-center justify-center'
                 key={Math.random()} onClick={() => onButtonClick(answer.value)}>{answer.title}
@@ -80,13 +88,7 @@ class Chatbot extends Component {
                         <img src={Brain} alt="" />
                     </div>
                     <div className='grid grid-rows-1  my-5 mx-5 bg-slate-100 rounded-md shadow-md py-6 dark:bg-slate-700'>
-                        {loading && !finished ? <h2 className='dark:text-slate-300 text-2xl w-full flex justify-center'>Loading...</h2> :
-                            <div>
-                                <h2 className='dark:text-slate-300 text-2xl w-full flex justify-center py-5'>Cannot connect to Server</h2>
-                                <div className="w-full flex justify-center"><img src={Error} alt=""/></div>
-                            </div>
-                        }
-
+                        {loading && !finished ? <h2 className='dark:text-slate-300 text-2xl w-full flex justify-center'>Loading...</h2> : null}
                         {!loading && !finished ? <SearchContainer question={question} answersButtons={answersButtons} onButtonClick={this.onButtonClick} /> : null}
                         {finished ? <FinishedContainer characterMatch={characterMatch} /> : null}
                     </div>
